f5574309be78fe810ea64a2a3117739db354d5da
ItsLxst <169390832+ItsLxst@users.noreply.github.com>
2026-08-22 17:53:04 +0300

    deploy fix

--- db.js
+++ db.js
@@ -1,18 +1,15 @@
 import pkg from "pg";
-const { Pool } = pkg;
 import dotenv from "dotenv";
+
+const { Pool } = pkg;
 
 dotenv.config();
 
 const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
+  connectionString: process.env.DATABASE_URL,
   ssl: {
     rejectUnauthorized: false
-}
+  }
 });
 
 pool.connect((err) => {
