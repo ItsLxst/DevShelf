65780ade864af205b08b946e92dbb3f89501d756
ItsLxst <169390832+ItsLxst@users.noreply.github.com>
2026-08-22 18:05:27 +0300

    fix database connection for Neon

--- db.js
+++ db.js
@@ -1,23 +1,20 @@
 import pkg from "pg";
 import dotenv from "dotenv";
 
-const { Pool } = pkg;
-
 dotenv.config();
 
+const { Pool } = pkg;
+
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false
-  }
+    connectionString: process.env.DATABASE_URL
 });
 
 pool.connect((err) => {
-  if (err) {
-    console.error("Error connecting to the database:", err);
-  } else {
-    console.log("Connected to the database");
-  }
+    if (err) {
+        console.error("Error connecting to the database:", err);
+    } else {
+        console.log("Connected to the database");
+    }
 });
 
 export default pool;
